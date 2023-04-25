150b89051837bf64349b8113a32492ed18a5430b
Zen Huifer <huifer97@163.com>
2023-04-25 10:11:22 +0800

    doc: 数据接受器

---
+++ warring/warring-start/src/main/java/com/github/huifer/hd/ws/Main.java
@@ -0,0 +1,11 @@
+package com.github.huifer.hd.ws;
+
+import java.time.LocalDateTime;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println(LocalDateTime.MIN);
+  }
+
+}
